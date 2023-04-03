2bee2fc3af0b19edeea64f60a03d1530757c9d61
Eric Enkhbold <ebemgl@yahoo.com>
2023-04-03 10:24:06 -0400

    Eric

--- src/test/java/stepDefinitions/DashboardSectionDefs_E.java
+++ src/test/java/stepDefinitions/DashboardSectionDefs_E.java
@@ -20,18 +20,18 @@ import static org.junit.Assert.assertTrue;
 public class DashboardSectionDefs_E extends HomePage {
 
     public class DashboardSteps {
-        @Given("^I am on the dashboard page$")
+        @Given("^User on the dashboard page$")
         public void navigateToDashboardPage() {
             Driver.getDriver().get( "http://qa-duobank.us-east-2.elasticbeanstalk.com/dashboard.php" );
         }
 
-        @Then("^I should see the bank's logo in the top left corner$")
+        @Then("^Bank logo should be displayed in the top left corner of the page$")
         public void verifyBankLogoDisplayed() {
             WebElement brandLogo = Driver.getDriver().findElement( By.xpath( "//div[@class='brand-logo']//span" ) );
             assertTrue( ((WebElement) brandLogo).isDisplayed() );
         }
 
-        @And("^I should see the \"([^\"]*)\" and \"([^\"]*)\" links$")  //matches any string of characters between two double quotes
+        @And("^User should see the \"([^\"]*)\" and \"([^\"]*)\" links$")  //matches any string of characters between two double quotes
         public void verifyLinksDisplayed(String link1, String link2) {
             List<WebElement> links = Driver.getDriver().findElements( By.xpath( "//div[@class='menu-title']//span" ) );
             List<String> expectedLinks = Arrays.asList( link1, link2 );
@@ -39,41 +39,41 @@ public class DashboardSectionDefs_E extends HomePage {
             assertEquals( expectedLinks, actualLinks );
         }
 
-        @When("^I click on the \"([^\"]*)\" link$")  //matches any string of characters between two double quotes
+        @When("^User click on the \"([^\"]*)\" link$")  //matches any string of characters between two double quotes
         public void clickOnLink(String link) {
             WebElement element = Driver.getDriver().findElement( By.xpath( "//div[@class='menu-title']//span[contains(text(),'" + link + "')]" ) );
             element.click();
         }
 
-        @Then("^I should be taken to a new page where I can apply for a new mortgage$")
+        @Then("^User should be taken to a new page where user can apply for a new mortgage$")
         public void verifyMortgageApplicationPage() {
             assertTrue( Driver.getDriver().getCurrentUrl().contains( "mortgage_application.php" ) );
         }
 
-        @Then("^I should be taken to a page where I can view my previous mortgage applications$")
+        @Then("^User should be taken to a page where user can view my previous mortgage applications$")
         public void verifyApplicationListPage() {
             assertTrue( Driver.getDriver().getCurrentUrl().contains( "application_list.php" ) );
         }
 
-        @And("^I should see my account information in the top right corner$")
+        @And("^User should see my account information in the top right corner$")
         public void verifyAccountInfoDisplayed() {
             WebElement userName = Driver.getDriver().findElement( By.xpath( "//span[@class='user-name']//span" ) );
             assertTrue( userName.isDisplayed() );
         }
 
-        @When("^I click on my name or profile picture$")
+        @When("^User click on my name or profile picture$")
         public void clickOnAccountInfo() {
             WebElement accountInfo = Driver.getDriver().findElement( By.xpath( "//span[@class='user-name']//span" ) );
             accountInfo.click();
         }
 
-        @Then("^I should see a dropdown menu with a \"Logout\" link$")
+        @Then("^User should see a dropdown menu with a \"Logout\" link$")
         public void verifyLogoutLinkDisplayed() {
             WebElement logOutLink = Driver.getDriver().findElement( By.xpath( "//div[@class='dropdown-item']//span[contains(text(),'Logout')]" ) );
             assertTrue( logOutLink.isDisplayed() );
         }
 
-        @And("^I should be able to click on the "Logout" link to log out of the system$")
+        @And("^User should be able to click on the "Logout" link to log out of the system$")
         public void clickOnLogoutLink() {
             WebElement logOutLink = Driver.getDriver().findElement( By.xpath( "//div[@class='dropdown-item']//span[contains(text(),'Logout')]" ) );
             logOutLink.click();
