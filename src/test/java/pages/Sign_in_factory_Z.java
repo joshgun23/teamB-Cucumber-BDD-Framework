8a701c78c26444289a89dc40d5ce2e44008d8686
Ziyoda Muslimova <ziyoda.muslimova@gmail.com>
2023-04-03 16:52:54 -0400

    commit

--- src/test/java/pages/Sign_in_factory_Z.java
+++ src/test/java/pages/Sign_in_factory_Z.java
@@ -8,108 +8,111 @@ import org.openqa.selenium.support.PageFactory;
 import utils.ConfigReader;
 import utils.Driver;
 
+import java.time.Duration;
+
 public class Sign_in_factory_Z {
     public Sign_in_factory_Z() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(name = "email")
+    @FindBy(id = "email")
     private WebElement eMail;
 
-    public void userName() {
-        eMail.click();
-        eMail.sendKeys(ConfigReader.getProperty("username"));
-    }
-
-    @FindBy(name = "password")
+    @FindBy(id = "password")
     private WebElement password;
 
-    public void passWord() {
-        password.sendKeys(ConfigReader.getProperty("password"));
-    }
-
-    @FindBy(name = "login")
-    private WebElement login;
-
-    public void loGin() {
-        login.click();
-    }
+    @FindBy(id = "signin")
+    public WebElement login;
 
     @FindBy(xpath = "//a[@class='dropdown-toggle']")
     private WebElement dashboard;
 
-    public void dashboard() {
-        Assert.assertTrue(dashboard.isDisplayed());
-
-
-    }
-
     @FindBy(name = "email")
     private WebElement invalidEmail;
-
-    public void invaliUserName() {
-        Faker faker = new Faker();
-        String email = faker.name().username() + "@gmail.com";
-        invalidEmail.sendKeys(email);
-
-
-    }
 
     @FindBy(name = "password")
     private WebElement invalidPassword;
 
-    public void invalidPassword() {
-        Faker faker = new Faker();
-        String password = faker.name().username();
-        invalidPassword.sendKeys(password);
-    }
-
     @FindBy(xpath = "//*[@id=\"auth-login\"]/div/div/div/div[1]/div/div[1]/text()")
     private WebElement errorMessage;
-
-    public void errorMessage() {
-        Assert.assertTrue(errorMessage.isDisplayed());
-    }
 
     @FindBy(name = "email")
     private WebElement noUserName;
 
-    public void noUserName() {
-        noUserName.sendKeys("");
-
-    }
     @FindBy(name = "password")
     private WebElement noPassword;
 
-    public void noPassword(){
-        noPassword.sendKeys("");
-    }
 
     @FindBy(xpath = "//button[@class='btn btn-primary btn-block btn-lg loginbtn']")
     private WebElement login2;
-    public void disabledButton() {
-        Assert.assertTrue(login.isEnabled());
 
-    }
     @FindBy(xpath = "//button[@class='btn btn-primary btn-block btn-lg loginbtn']")
     private WebElement login3;
-    public void missingField() {
-        Assert.assertTrue(login.isEnabled());
+
+    @FindBy(id = "hideLogin")
+    public WebElement signUpLink;
+
+    public void username(String email) {
+        eMail.sendKeys(email);
+
+
     }
 
-    @FindBy (id = "hideLogin")
-    private WebElement signUpLink;
+    public void password(String pass) {
+        password.sendKeys(pass);
+    }
 
-    public void signUpLink() {
+    public WebElement geteMail() {
+        return eMail;
+    }
+
+    public WebElement getPassword() {
+        return password;
+    }
+
+    public void SignIn() {
+        eMail.sendKeys(ConfigReader.getProperty("homepage"));
+        password.sendKeys(ConfigReader.getProperty("password"));
+        login.click();
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    }
+
+    public void invalidEmail() {
+        Faker faker = new Faker();
+        invalidEmail.sendKeys(faker.internet().emailAddress());
+        password.sendKeys(ConfigReader.getProperty("password"));
+        login.click();
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+    }
+
+    public void invalidPassword() {
+        Faker faker = new Faker();
+        invalidPassword.sendKeys(faker.internet().password());
+        eMail.sendKeys(ConfigReader.getProperty("homepage"));
+        login.click();
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    }
+
+    public void noUserName() {
+        noUserName.sendKeys("");
+        password.sendKeys(ConfigReader.getProperty("password"));
+        login.click();
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    }
+
+    public void noPassword() {
+        eMail.sendKeys(ConfigReader.getProperty("homepage"));
+        noPassword.sendKeys("");
+        login.click();
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+    }
+
+    public void errorMessage() {
+        Assert.assertEquals("Invalid email or password", errorMessage.getText());
+    }
+
+
+   public void SighnUpLink() {
         signUpLink.click();
-
-    }
-
-    public void signUpPage() {
-        Assert.assertTrue(signUpLink.isDisplayed());
-    }
-
-
-    public void forgotPasswordLink() {
-    }
-}
+    }}
--- src/test/resources/user_login_Z.feature
+++ src/test/resources/user_login_Z.feature
@@ -2,33 +2,27 @@
 Feature: Sign in to mortgage account
 
 
+    Scenario : User signs in with valid credentials
+	Given the user is on the sign in page
+	When the user enters a valid email address and password
+	When the user clicks on the sign in button
+	Then the user should be redirected to the mortgage account dashboard
 
-  Scenario 1 User signs in with valid credentials
-    Given the user is on the sign in page
-    When the user enters a valid email address and password
-    And clicks on the sign in button
-    Then the user should be redirected to the mortgage account dashboard
-
-  Scenario 2 User signs in with invalid credentials
-    Given the user is on the sign in page
+  Scenario : User signs in with invalid credentials
     When the user enters an invalid email address or password
-    And clicks on the sign in button
-    Then the user should see an error message "Login Failed"
+	Then the user should see an error message "Login Failed"
 
-  Scenario 3 User attempts to sign in with missing credentials
-    Given the user is on the sign in page
+  Scenario : User attempts to sign in with missing credentials
     When the user attempts to sign in without entering an email address or password
-    Then the sign in button should be disabled
-    And the user should see an error message indicating the missing field
+	Then the sign in button should be disabled
+	And the user should see an error message indicating the missing field
 
-  Scenario 4 User navigates to sign up page
-    Given the user is on the sign in page
+  Scenario : User navigates to sign up page
     When the user clicks the "Don't have an account? Sign up" link
-    Then the user should be redirected to the sign up page
+	Then the user should be redirected to the sign up page
 
-  Scenario 5 User navigates to forgot password page
-    Given the user is on the sign in page
+  Scenario : User navigates to forgot password page
     When the user clicks the "Forgot your password?" link
-    Then the user should be redirected to the forgot password page
-    And the user should see a form to reset their password.
+	Then the user should be redirected to the forgot password page
+	And the user should see a form to reset their password.
 
