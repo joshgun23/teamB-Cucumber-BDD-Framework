8dbb2dde783a511a0f090c6dd70d1b24c1928960
Joshgun <Coshgun.ismayilov231985@gmail.com>
2023-04-13 18:45:13 -0400

    second

--- src/test/java/stepDefinitions/db/Personal_Info_DB_C_test.java
+++ src/test/java/stepDefinitions/db/Personal_Info_DB_C_test.java
@@ -1,7 +1,48 @@
 package stepDefinitions.db;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Personal_Info_DB_C_test {
 
+    @When("I enter the borrower's information:")
+    public void i_enter_the_borrower_s_information(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+//    @When("I select {string} for co-borrower question")
+//    public void i_select_for_co_borrower_question(String string) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @When("I enter the co-borrower's information:")
+//    public void i_enter_the_co_borrower_s_information(io.cucumber.datatable.DataTable dataTable) {
+//        // Write code here that turns the phrase above into concrete actions
+//        // For automatic transformation, change DataTable to one of
+//        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+//        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+//        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+//        //
+//        // For other transformations you can register a DataTableType.
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @When("I submit the form")
+//    public void i_submit_the_form() {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
+//    @Then("the borrower's and co-borrower's information should be stored in the {string} table in the database")
+//    public void the_borrower_s_and_co_borrower_s_information_should_be_stored_in_the_table_in_the_database(String string) {
+//        // Write code here that turns the phrase above into concrete actions
+//        throw new io.cucumber.java.PendingException();
+//    }
 
 
 }
