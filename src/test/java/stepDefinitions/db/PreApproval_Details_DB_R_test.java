d1ac4703896b35e8d8b656a62e155c00b998952c
Renata Saudargaite <rsaudar@gmail.com>
2023-04-24 14:55:57 -0400

    PreApproval Details changes

--- src/test/java/stepDefinitions/db/PreApproval_Details_DB_R_test.java
+++ src/test/java/stepDefinitions/db/PreApproval_Details_DB_R_test.java
@@ -1,5 +1,6 @@
 package stepDefinitions.db;
 
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -17,102 +18,261 @@ public class PreApproval_Details_DB_R_test {
     @Given("user navigates to the SignIn page")
     public void user_navigates_to_the_sign_in_page() {
     }
+
     @When("user enters a valid email address and password and clicks on Sing In button")
     public void user_enters_a_valid_email_address_and_password_and_clicks_on_sing_in_button() {
-        PreApproval_Details_Page_R preApproval_details_page = new PreApproval_Details_Page_R();
-        preApproval_details_page.LogInMethod();
+        PreApproval_Details_Page_R page = new PreApproval_Details_Page_R();
+        page.LogInMethod();
         SeleniumUtils.waitFor(5);
     }
+
     @Then("user is redirected to the Dashboard page")
     public void user_is_redirected_to_the_dashboard_page() {
     }
+
     @When("user clicks on Mortgage Application button")
     public void user_clicks_on_mortgage_application_button() {
-        PreApproval_Details_Page_R preApproval_details_page = new PreApproval_Details_Page_R();
+        PreApproval_Details_Page_R page = new PreApproval_Details_Page_R();
         SeleniumUtils.waitFor(10);
-        preApproval_details_page.clickMortgageApplication();
+        page.clickMortgageApplication();
     }
+
     @Then("user is redirected to the PreApproval Details page")
     public void user_is_redirected_to_the_pre_approval_details_page() {
     }
+
     @Given("user is on the PreApproval Details page")
     public void user_is_on_the_pre_approval_details_page() {
     }
+
     @When("user fills out PreApproval Details page form fields using valid information")
     public void user_fills_out_preapproval_details_page_form_fields_using_valid_information(List<Map<String, String>> dataTable) {
-        Map<String, String> data = dataTable.get(0);
-        new PreApproval_Details_Page_R().fillOutTheForm(
-        data.get("realtor_status"),
-        data.get("realtor_info"),
-        data.get("loan_officer_status"),
-        data.get("purpose_loan"),
-        data.get("est_purchase_price"),
-        data.get("down_payment"),
-        data.get("down_payment_percent"),
-        data.get("total_loan_amount"),
-        data.get("src_down_payment"),
-        data.get("add_fund_available"));
-        new PreApproval_Details_Page_R().clickNext();
-        new PreApproval_Details_Page_R().personalInfo();
-        new PreApproval_Details_Page_R().fillOutExpensesForm();
+        Map<String, String> formData = dataTable.get(0);
+        PreApproval_Details_Page_R page = new PreApproval_Details_Page_R();
+        page.fillOutTheForm(
+                formData.get("realtor_status"),
+                formData.get("realtor_info"),
+                formData.get("loan_officer_status"),
+                formData.get("purpose_loan"),
+                formData.get("est_purchase_price"),
+                formData.get("down_payment"),
+                formData.get("down_payment_percent"),
+                formData.get("total_loan_amount"),
+                formData.get("src_down_payment"),
+                formData.get("add_fund_available"));
+        page.clickNext();
+        page.personalInfo();
+        page.fillOutExpensesForm();
         new Employment_And_Income_Page_R().passEmploymentPage();
-        new PreApproval_Details_Page_R().fillingCreditReport();
-        new PreApproval_Details_Page_R().fillingOutEconsent();
-        new PreApproval_Details_Page_R().clickSubmit();
+        page.fillingCreditReport();
+        page.fillingOutEconsent();
+        page.clickSubmit();
         SeleniumUtils.waitFor(5);
     }
-    private String getLastId(){
+
+    private String getLastId() {
         List<List<Object>> dbResults = DBUtils.getListOfLists("SELECT MAX(id) from tbl_mortagage");
         return dbResults.get(0).get(0).toString();
     }
-    @Then("entered details for Mortgage PreApproval can be viewed by the user in their respective columns")
-    public void entered_details_for_mortgage_preapproval_can_be_viewed_by_the_user_in_their_respective_columns(List<Map<String, String>> dataTable) throws SQLException {
-        String actualId = null;
+
+    @Then("verify mortgage preapproval details are stored in the table")
+    public void verify_mortgage_preapproval_details_are_stored_in_the_table(List<Map<String, String>> dataTable) throws SQLException {
+        String actualId = getLastId();
         try {
-            actualId = getLastId();
             List<Map<String, Object>> actual = DBUtils.getListOfMaps("SELECT * FROM tbl_mortagage WHERE id = " + actualId);
-            Map<String, String> expected = dataTable.get(0);
-
-            String expectedRealtorStatus = expected.get("realtor_status");
-            String expectedRealtorInfo = expected.get("realtor_info");
-            String expectedLoanOfficerStatus = expected.get("loan_officer_status");
-            String expectedPurposeLoan = expected.get("purpose_loan");
-            String expectedEstPrice = expected.get("est_purchase_price");
-            String expectedDownPayment = expected.get("down_payment");
-            String expectedDownPaymentPercent = expected.get("down_payment_percent");
-            String expectedTotalLoanAmount = expected.get("total_loan_amount");
-            String expectedSrcDownPayment = expected.get("src_down_payment");
-            String expectedAddFundAvailable = expected.get("add_fund_available");
-
-            String actualRealtorStatus = actual.get(Integer.parseInt("realtor_status")).toString();
-            String actualRealtorInfo = actual.get(Integer.parseInt("realtor_info")).toString();
-            String actualLoanOfficerStatus = actual.get(Integer.parseInt("loan_officer_status")).toString();
-            String actualPurposeLoan = actual.get(Integer.parseInt("purpose_loan")).toString();
-            String actualEstPrice = actual.get(Integer.parseInt("est_purchase_price")).toString();
-            String actualDownPayment = actual.get(Integer.parseInt("down_payment")).toString();
-            String actualDownPaymentPercent = actual.get(Integer.parseInt("down_payment_percent")).toString();
-            String actualTotalLoanAmount = actual.get(Integer.parseInt("total_loan_amount")).toString();
-            String actualSrcDownPayment = actual.get(Integer.parseInt("src_down_payment")).toString();
-            String actualAddFundAvailable = actual.get(Integer.parseInt("add_fund_available")).toString();
-
+            Map<String, String> expectedData = dataTable.get(0);
             SoftAssertions softAssertions = new SoftAssertions();
-            softAssertions.assertThat(actualRealtorStatus).isEqualTo(expectedRealtorStatus);
-            softAssertions.assertThat(actualRealtorInfo).isEqualTo(expectedRealtorInfo);
-            softAssertions.assertThat(actualLoanOfficerStatus).isEqualTo(expectedLoanOfficerStatus);
-            softAssertions.assertThat(actualPurposeLoan).isEqualTo(expectedPurposeLoan);
-            softAssertions.assertThat(actualEstPrice).isEqualTo(expectedEstPrice);
-            softAssertions.assertThat(actualDownPayment).isEqualTo(expectedDownPayment);
-            softAssertions.assertThat(actualDownPaymentPercent).isEqualTo(expectedDownPaymentPercent);
-            softAssertions.assertThat(actualTotalLoanAmount).isEqualTo(expectedTotalLoanAmount);
-            softAssertions.assertThat(actualSrcDownPayment).isEqualTo(expectedSrcDownPayment);
-            softAssertions.assertThat(actualAddFundAvailable).isEqualTo(expectedAddFundAvailable);
+            softAssertions.assertThat(actual.get(0).get("realtor_status")).isEqualTo(Integer.parseInt(expectedData.get("realtor_status")));
+            softAssertions.assertThat((String) actual.get(0).get("realtor_info")).isEqualTo(expectedData.get("realtor_info"));
+            softAssertions.assertThat(actual.get(0).get("loan_officer_status")).isEqualTo(Integer.parseInt(expectedData.get("loan_officer_status")));
+            softAssertions.assertThat((String) actual.get(0).get("purpose_loan")).isEqualTo(expectedData.get("purpose_loan"));
+            softAssertions.assertThat((String) actual.get(0).get("est_purchase_price")).isEqualTo(expectedData.get("est_purchase_price"));
+            softAssertions.assertThat((String) actual.get(0).get("down_payment")).isEqualTo(expectedData.get("down_payment"));
+            softAssertions.assertThat((String) actual.get(0).get("down_payment_percent")).isEqualTo(expectedData.get("down_payment_percent"));
+            softAssertions.assertThat((String) actual.get(0).get("total_loan_amount")).isEqualTo(expectedData.get("total_loan_amount"));
+            softAssertions.assertThat((String) actual.get(0).get("src_down_payment")).isEqualTo(expectedData.get("src_down_payment"));
+            softAssertions.assertThat((String) actual.get(0).get("add_fund_available")).isEqualTo(expectedData.get("add_fund_available"));
             softAssertions.assertAll();
         } finally {
-            DBUtils.executeUpdate("DELETE FROM tbl_mortagage WHERE id =" + actualId);
+            DBUtils.executeUpdate("DELETE FROM tbl_mortagage WHERE id=" + actualId);
         }
     }
-}
 
+    @When("user selects checkboxes for {string} and {string} sections in the PreApproval details page")
+    public void user_selects_checkboxes_for_and_sections_in_the_pre_approval_details_page(String realtor_status_checkbox, String loan_officer_status_checkbox) {
+        Faker faker = new Faker();
+        PreApproval_Details_Page_R page = new PreApproval_Details_Page_R();
+        page.fillOutTheForm(realtor_status_checkbox,
+                faker.name().fullName(),
+                loan_officer_status_checkbox,
+                "Purchase a Home",
+                "700000",
+                "50000",
+                "7",
+                "650000",
+                "Other type of Down Payment",
+                "35000");
+        page.clickNext();
+        page.personalInfo();
+        page.fillOutExpensesForm();
+        new Employment_And_Income_Page_R().passEmploymentPage();
+        page.fillingCreditReport();
+        page.fillingOutEconsent();
+        page.clickSubmit();
+        SeleniumUtils.waitFor(5);
+    }
+
+    @Then("the corresponding integer values should be stored in the {string} and {string} columns respectively")
+    public void the_corresponding_integer_values_should_be_stored_in_the_and_columns_respectively(String realtor_status, String loan_officer_status) throws SQLException {
+        String actualId = getLastId();
+        try {
+            List<Map<String, Object>> actual = DBUtils.getListOfMaps("SELECT * FROM tbl_mortagage where id=" + actualId);
+            SoftAssertions softAssertions = new SoftAssertions();
+            softAssertions.assertThat(actual.get(0).get("realtor_status")).isEqualTo(Integer.parseInt(realtor_status));
+            softAssertions.assertThat(actual.get(0).get("loan_officer_status")).isEqualTo(Integer.parseInt(loan_officer_status));
+            softAssertions.assertAll();
+        } finally {
+            DBUtils.executeUpdate("DELETE FROM tbl_mortagage where id=" + actualId);
+        }
+    }
+
+    @When("user enters the following information in the realtor_info, purpose_loan, and src_down_payment columns in the PreApproval details page:")
+    public void user_enters_the_following_information_in_the_realtor_info_purpose_loan_and_src_down_payment_columns_in_the_pre_approval_details_page(List<Map<String, String>> dataTable) {
+        Map<String, String> formData = dataTable.get(0);
+        PreApproval_Details_Page_R page = new PreApproval_Details_Page_R();
+        page.fillOutTheForm(
+                "Yes",
+                formData.get("realtor_info"),
+                "Yes",
+                formData.get("purpose_loan"),
+                "700000",
+                "50000",
+                "7",
+                "650000",
+                formData.get("src_down_payment"),
+                "35000");
+        page.clickNext();
+        page.personalInfo();
+        page.fillOutExpensesForm();
+        new Employment_And_Income_Page_R().passEmploymentPage();
+        page.fillingCreditReport();
+        page.fillingOutEconsent();
+        page.clickSubmit();
+        SeleniumUtils.waitFor(5);
+    }
+
+    @Then("all entered values should be stored as a String values")
+    public void all_entered_values_should_be_stored_as_a_string_values() throws SQLException {
+        String actualId = getLastId();
+        try {
+            List<Map<String, Object>> actual = DBUtils.getListOfMaps("SELECT * FROM tbl_mortagage where id=" + actualId);
+            SoftAssertions softAssertions = new SoftAssertions();
+            softAssertions.assertThat(actual.get(0).get("realtor_info") instanceof String);
+
+            softAssertions.assertThat(actual.get(0).get("purpose_loan") instanceof String);
+
+            softAssertions.assertThat(actual.get(0).get("src_down_payment") instanceof String);
+
+            softAssertions.assertAll();
+        }finally{
+            DBUtils.executeUpdate("DELETE FROM tbl_mortagage where id=" + actualId);
+        }
+    }
+    @When("user inputs the following information for the est_purchase_price, down_payment, down_payment_percent, total_loan_amount, and add_fund_available columns")
+    public void user_inputs_the_following_information_for_the_est_purchase_price_down_payment_down_payment_percent_total_loan_amount_and_add_fund_available_columns(List<Map<String, String>> dataTable) {
+        Map<String, String> formData = dataTable.get(0);
+        PreApproval_Details_Page_R page = new PreApproval_Details_Page_R();
+        page.fillOutTheForm(
+                "Yes",
+                "Keller Williams",
+                "Yes",
+                "Purchase a Home",
+                formData.get("est_purchase_price"),
+                formData.get("down_payment"),
+                formData.get("down_payment_percent"),
+                formData.get("total_loan_amount"),
+                "Other type of Down Payment",
+                formData.get("add_fund_available"));
+        page.clickNext();
+        page.personalInfo();
+        page.fillOutExpensesForm();
+        new Employment_And_Income_Page_R().passEmploymentPage();
+        page.fillingCreditReport();
+        page.fillingOutEconsent();
+        page.clickSubmit();
+        SeleniumUtils.waitFor(5);
+    }
+
+    @Then("all entered values should be stored as integers in the system")
+    public void all_entered_values_should_be_stored_as_integers_in_the_system() throws SQLException {
+        String actualId = getLastId();
+        try {
+            List<Map<String, Object>> actual = DBUtils.getListOfMaps("SELECT * FROM tbl_mortagage where id=" + actualId);
+            SoftAssertions softAssertions = new SoftAssertions();
+            softAssertions.assertThat(actual.get(0).get("est_purchase_price") instanceof Integer);
+            softAssertions.assertThat(actual.get(0).get("down_payment") instanceof Integer);
+            softAssertions.assertThat(actual.get(0).get("down_payment_percent") instanceof Integer);
+            softAssertions.assertThat(actual.get(0).get("total_loan_amount") instanceof Integer);
+            softAssertions.assertThat(actual.get(0).get("add_fund_available") instanceof Integer);
+            softAssertions.assertAll();
+        }finally{
+            DBUtils.executeUpdate("DELETE FROM tbl_mortagage where id=" + actualId);
+        }
+    }
+    @When("user enters the following data {string} , {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string} into the PreApproval Details page form fields")
+    public void user_enters_the_following_data_into_the_pre_approval_details_page_form_fields(String realtor_status, String realtor_info, String loan_officer_status,
+                                                                                              String purpose_loan, String est_purchase_price, String down_payment,
+                                                                                              String down_payment_percent, String total_loan_amount,
+                                                                                              String src_down_payment, String add_fund_available) {
+        PreApproval_Details_Page_R page = new PreApproval_Details_Page_R();
+        page.fillOutTheForm(
+                realtor_status,
+                realtor_info,
+                loan_officer_status,
+                purpose_loan,
+                est_purchase_price,
+                down_payment,
+                down_payment_percent,
+                total_loan_amount,
+                src_down_payment,
+                add_fund_available);
+        page.clickNext();
+        page.personalInfo();
+        page.fillOutExpensesForm();
+        new Employment_And_Income_Page_R().passEmploymentPage();
+        page.fillingCreditReport();
+        page.fillingOutEconsent();
+        page.clickSubmit();
+        SeleniumUtils.waitFor(5);
+    }
+    @Then("the data should be correctly mapped to the following column names {string} , {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string} in the tbl_mortagage table:")
+    public void the_data_should_be_correctly_mapped_to_the_following_column_names_in_the_tbl_mortagage_table(String realtor_status, String realtor_info, String loan_officer_status,
+                                                                                                             String purpose_loan, String est_purchase_price, String down_payment,
+                                                                                                             String down_payment_percent, String total_loan_amount, String src_down_payment,
+                                                                                                             String add_fund_available) throws SQLException {
+        String actualId = getLastId();
+        try {
+            List<Map<String, Object>> actual = DBUtils.getListOfMaps("SELECT * FROM tbl_mortagage where id=" + actualId);
+            SoftAssertions softAssertions = new SoftAssertions();
+            softAssertions.assertThat( actual.get(0).get("realtor_status")).isEqualTo(Integer.parseInt(realtor_status));
+            softAssertions.assertThat((String) actual.get(0).get("realtor_info")).isEqualTo(realtor_info);
+            softAssertions.assertThat( actual.get(0).get("loan_officer_status")).isEqualTo(Integer.parseInt(loan_officer_status));
+            softAssertions.assertThat((String) actual.get(0).get("purpose_loan")).isEqualTo(purpose_loan);
+            softAssertions.assertThat((String) actual.get(0).get("est_purchase_price")).isEqualTo(est_purchase_price);
+            softAssertions.assertThat((String) actual.get(0).get("down_payment")).isEqualTo(down_payment);
+            softAssertions.assertThat((String) actual.get(0).get("down_payment_percent")).isEqualTo(down_payment_percent);
+            softAssertions.assertThat((String) actual.get(0).get("total_loan_amount")).isEqualTo(total_loan_amount);
+            softAssertions.assertThat((String) actual.get(0).get("src_down_payment")).isEqualTo(src_down_payment);
+            softAssertions.assertThat((String) actual.get(0).get("add_fund_available")).isEqualTo(add_fund_available);
+
+            softAssertions.assertAll();
+        }finally{
+            DBUtils.executeUpdate("DELETE FROM tbl_mortagage where id=" + actualId);
+        }
+
+    }
+
+}
 
 
 
--- src/test/resources/db_teamB/pre-approval_details_DB_R_test.feature
+++ src/test/resources/db_teamB/pre-approval_details_DB_R_test.feature
@@ -16,10 +16,42 @@
     |realtor_status  |realtor_info    |loan_officer_status|purpose_loan    |est_purchase_price|down_payment|down_payment_percent|total_loan_amount|src_down_payment           |add_fund_available|
     |yes             |Keller Williams |yes                |Purchase a Home |40000             |10000       |25                  |30000            |Other type of Down Payment |5000              |
 
-    Then entered details for Mortgage PreApproval can be viewed by the user in their respective columns
+    Then verify mortgage preapproval details are stored in the table
     |realtor_status  |realtor_info    |loan_officer_status|purpose_loan    |est_purchase_price|down_payment|down_payment_percent|total_loan_amount|src_down_payment           |add_fund_available|
     |1               |Keller Williams |1                  |Purchase a Home |40000             |10000       |25                  |30000            |Other type of Down Payment |5000              |
 
+    @db_preapp
+  Scenario Outline: Storing realtor_status and loan_officer column values as integers
+    Given user is on the PreApproval Details page
+    When user selects checkboxes for "<realtor_status_checkbox>" and "<loan_officer_status_checkbox>" sections in the PreApproval details page
+    Then the corresponding integer values should be stored in the "<realtor_status>" and "<loan_officer_status>" columns respectively
+    Examples:
+    | realtor_status_checkbox  |loan_officer_status_checkbox| realtor_status  |loan_officer_status|
+    |yes                       |yes                         |1                |  1                |
+    |no                        |no                          |2                |  2                |
 
+    @db_preapp
+  Scenario: Store realtor_info, purpose_loan, and src_down_payment as String values
+    Given user is on the PreApproval Details page
+    When user enters the following information in the realtor_info, purpose_loan, and src_down_payment columns in the PreApproval details page:
+    |realtor_info    |purpose_loan    |src_down_payment           |
+    |Keller Williams |Purchase a Home |Other type of Down Payment |
+    Then all entered values should be stored as a String values
 
+    @db_preapp
+  Scenario: Store PreApproval Details as Integer Values
+    Given user is on the PreApproval Details page
+    When user inputs the following information for the est_purchase_price, down_payment, down_payment_percent, total_loan_amount, and add_fund_available columns
+    |est_purchase_price|down_payment|down_payment_percent|total_loan_amount|add_fund_available|
+    |700000            |50000       |7                   |650000           |35000             |
+    Then all entered values should be stored as integers in the system
 
+    @db_preapp
+  Scenario Outline: Check that data from PreApproval Details page is mapped correctly to the database table
+    Given user is on the PreApproval Details page
+    When user enters the following data "<realtor_status>" , "<realtor_info>", "<loan_officer_status>", "<purpose_loan>", "<est_purchase_price>", "<down_payment>", "<down_payment_percent>", "<total_loan_amount>", "<src_down_payment>", "<add_fund_available>" into the PreApproval Details page form fields
+    Then the data should be correctly mapped to the following column names "<realtor_status>" , "<realtor_info>", "<loan_officer_status>", "<purpose_loan>", "<est_purchase_price>", "<down_payment>", "<down_payment_percent>", "<total_loan_amount>", "<src_down_payment>", "<add_fund_available>" in the tbl_mortagage table:
+    Examples:
+    |realtor_status  |realtor_info    |loan_officer_status  |purpose_loan    |est_purchase_price|down_payment|down_payment_percent|total_loan_amount|src_down_payment                                |add_fund_available|
+    |1               |Keller Williams |1                    |Purchase a Home |250765.00         |45234       |18                  |205531           |Equity on Pending Sale (executed sales contract)|50000             |
+    |1               |Douglas Elliman |2                    |Purchase a Home |946466            |56907       |6                   |889559           |Equity on Pending Sale (executed sales contract)|15000             |
